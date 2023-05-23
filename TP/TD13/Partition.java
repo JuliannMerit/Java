6beb3575aff0efb38a7dc8c980215aa5268e3d14
Jujua_c <juliann.merit@gmail.com>
2023-05-23 10:59:22 +0200

    Update Partition.java

--- TP/TD13/Partition.java
+++ TP/TD13/Partition.java
@@ -4,9 +4,6 @@ public class Partition extends ArrayList<ElementMusical> {
     public Partition(){
         super();
     }
-    public void ajouter(ElementMusical e){
-        super.add(e);
-    }
     public void jouer(){
         for (ElementMusical e : this){
             e.jouer();
@@ -15,14 +12,14 @@ public class Partition extends ArrayList<ElementMusical> {
 
     public static void main(String [] args){
         Partition p = new Partition();
-        p.ajouter(new Note (440, 1));
-        p.ajouter(new Note (880, 1));
-        p.ajouter(new Note (1760, 1));
+        p.add(new Note (440, 1));
+        p.add(new Note (880, 1));
+        p.add(new Note (1760, 1));
         Accord accord = new  Accord();
         accord.add(new Note (440, 1));
         accord.add(new Note (880, 1));
         accord.add(new Note (1760, 1));
-        p.ajouter(accord);
+        p.add(accord);
         p.jouer();
     }
 }
